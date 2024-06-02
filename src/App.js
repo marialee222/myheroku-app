4a95a3df0f1e3eb6025be18b22990f6f39ba8563
Maria Lee <juggleisreal@gmail.com>
2024-06-01 22:23:00 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to My Custom React App</h1>
+        <p>
+          This is a custom message to replace the default React placeholder.
+        </p>
+        <p>
+          Let's build something amazing!
+        </p>
+        <a
+          className="App-link"
+          href="https://github.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Visit My GitHub
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
